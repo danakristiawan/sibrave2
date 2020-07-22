33320e04df49dffa21b20a97a2d7f3e40f280c44
danakristiawan <danakristiawan@gmail.com>
2020-07-22 17:18:40 +0700

    add jenis view

---
+++ application/controllers/Jenis.php
@@ -0,0 +1,98 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Jenis extends CI_Controller
+{
+  public function __construct()
+  {
+    parent::__construct();
+    is_logged_in();
+    $this->load->model('judul_model', 'judul');
+  }
+
+  public function index()
+  {
+    // data
+    $data['title'] = $this->judul->title();
+    $data['jenis'] = $this->db->get('data_jenis')->result_array();
+    // form
+    $this->load->view('template/header');
+    $this->load->view('template/sidebar', $data);
+    $this->load->view('template/topbar', $data);
+    $this->load->view('jenis/index', $data);
+    $this->load->view('template/footer');
+  }
+
+  public function add()
+  {
+    //providing data
+    $data['title'] = $this->judul->title();
+    // validasi
+    $rules = [
+      [
+        'field' => 'nama',
+        'label' => 'Nama',
+        'rules' => 'required|trim'
+      ]
+    ];
+    $validation = $this->form_validation->set_rules($rules);
+    if ($validation->run()) {
+      //query
+      $data = [
+        'nama' => htmlspecialchars($this->input->post('nama', true)),
+        'date_created' => time()
+      ];
+      $this->db->insert('data_jenis', $data);
+      redirect('jenis');
+    }
+    // form
+    $this->load->view('template/header');
+    $this->load->view('template/sidebar', $data);
+    $this->load->view('template/topbar', $data);
+    $this->load->view('jenis/add', $data);
+    $this->load->view('template/footer');
+  }
+
+  public function edit($id = null)
+  {
+    // check id
+    if (!isset($id)) redirect('auth/blocked');
+    // data
+    $data['title'] = $this->judul->title();
+    $data['jenis'] = $this->db->get_where('data_jenis', ['id' => $id])->row_array();
+    // validasi
+    $rules = [
+      [
+        'field' => 'nama',
+        'label' => 'Nama',
+        'rules' => 'required|trim'
+      ]
+    ];
+    $validation = $this->form_validation->set_rules($rules);
+    if ($validation->run()) {
+      //query
+      $data = [
+        'nama' => htmlspecialchars($this->input->post('nama', true)),
+        'date_created' => time()
+      ];
+      $this->db->update('data_jenis', $data, ['id' => $id]);
+      redirect('jenis');
+    }
+    // form
+    $this->load->view('template/header');
+    $this->load->view('template/sidebar', $data);
+    $this->load->view('template/topbar', $data);
+    $this->load->view('jenis/edit', $data);
+    $this->load->view('template/footer');
+  }
+
+  public function delete($id = null)
+  {
+    // cek id
+    if (!isset($id)) redirect('auth/blocked');
+    // query
+    if ($this->db->delete('data_jenis', ['id' => $id])) {
+      redirect('jenis');
+    }
+  }
+}
---
+++ application/views/jenis/add.php
@@ -0,0 +1,29 @@
+<section>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="card">
+          <form action="" method="post" autocomplete="off">
+            <div class="card-header">
+
+            </div>
+            <div class="card-body">
+              <div class="form-group">
+                <label for="">Nama :</label>
+                <input type="text" name="nama" class="form-control <?= form_error('nama') ? 'is-invalid' : '' ?>" value="<?= set_value('nama'); ?>">
+                <div class="invalid-feedback">
+                  <?= form_error('nama') ?>
+                </div>
+              </div>
+
+            </div>
+            <div class="card-footer">
+              <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Simpan</button>
+            </div>
+          </form>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</section>
