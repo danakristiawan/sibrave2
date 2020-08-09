c440245f68f757405bf3d1cbd73569faec21fc8a
danakristiawan <danakristiawan@gmail.com>
2020-08-09 10:47:44 +0700

    fix blocked-all view bug

--- application/views/auth/blocked-all.php
+++ application/views/auth/blocked-all.php
@@ -4,7 +4,8 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 3 | Lockscreen</title>
+  <title>Aplikasi Sibrave</title>
+  <link rel="shortcut icon" href="<?= base_url(); ?>assets/img/sibrave.png" type="image/x-icon">
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
